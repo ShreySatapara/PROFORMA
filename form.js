a04ae8dcf22ed2c6975e8924075add86d00298dc
Shrey Satapara <shrey.satapara.99@gmail.com>
2021-10-16 01:19:54 +0530

    validation tab1

--- form.js
+++ form.js
@@ -42,23 +42,42 @@ function nextPrev(n) {
 }
 
 function validateForm() {
-    /*var x, y, i,
-    
+    var x, y, i, valid = true;
     x = document.getElementsByClassName("tab");
     y = x[currentTab].getElementsByTagName("input");
-    console.log(y);
+
     for (i = 0; i < y.length; i++) {
-        if (y[i].value == "") {
-            y[i].className += " invalid";
-            valid = false;
-        } else if (y[i].name == "gender") {
-            if (!(y[i].checked)) {
-                y[i].className += " invalid"
+        if (!y[i].classList.contains("ignore_invalid")) {
+            if (y[i].type == "radio") {
+                var radios = document.getElementsByName(y[i].name);
+                var isChecked = false;
+                var j = 0;
+                while (!isChecked && j < radios.length) {
+                    if (radios[j].checked) isChecked = true;
+                    j++;
+                }
+                if (!isChecked) {
+                    y[i].parentElement.parentElement.className += " border border-danger";
+                    valid = false;
+                }
+            } else if (y[i].value == "" && y[i].type != "radio") {
+                y[i].className += " border-danger";
+                valid = false;
             }
         }
-    }*/
-    valid = true;
-    if (valid) { document.getElementsByClassName("step")[currentTab].className += " finish"; }
+    }
+
+    var clg_name_select = document.getElementById("clg_name");
+    if (clg_name_select.value == "") {
+        clg_name_select.classList.add("border-danger");
+    }
+
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+        document.getElementById("fill_warning").setAttribute("hidden", true);
+    } else {
+        document.getElementById("fill_warning").removeAttribute("hidden");
+    }
     return valid;
 }
 
